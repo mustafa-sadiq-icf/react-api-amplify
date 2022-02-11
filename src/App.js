8a4f2163ba9a30ba7970fb9f3076130c88275346
mustafa-sadiq-icf <mustafa.sadiq@icf.com>
2022-02-10 23:29:31 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function fetchAPI(param) {
+  return fetch("https://api.github.com/users/" + param).then(res => res.json())
+    .then((data) => {
+      return data
+    })
+    .catch(console.log)
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      username: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    fetchAPI(this.state.username).then(result => {
+      this.setState({ result: result });
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            GitHub username:
+            <textarea value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        {this.state.result && <div><pre>{JSON.stringify(this.state.result, null, 2)}</pre></div>}
+
+      </div>
+    );
+  }
 }
 
 export default App;
